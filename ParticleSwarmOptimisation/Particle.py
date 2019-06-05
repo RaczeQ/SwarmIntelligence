6a8698c656de04b7cc10317022841115d5ac4c78
Anna Makuś <anm@dsr.com.pl>
2019-06-05 17:04:52 +0200

    Explore neighborhood in PSO

--- ParticleSwarmOptimisation/Particle.py
+++ ParticleSwarmOptimisation/Particle.py
@@ -3,20 +3,22 @@ sys.path.insert(1, '.')
 
 from Optimisation.Optimizer import Optimizer
 from Optimisation.Entity import Entity
-
+import random
 
 class Particle(Entity):
 
-    def __init__(self, velocity, objective_function):
+    def __init__(self, objective_function, weight, c1, c2):
         super().__init__(objective_function)
         
-
-    def set_entity_initial_parameters(self, velocity):
+    def set_entity_initial_parameters(self, weight, c1, c2):
         super().set_entity_initial_parameters()
-        self.velocity = velocity
+        self.weight = weight
+        self.c1 = c1
+        self.c2 = c2
         self.p_best_x = self.x
         self.p_best_y = self.y
         self.p_best_fitness = self.fitness
+        self.velocity_x, self.velocity_y = self.objective_function.sample_position()
 
     def explore_neighborhood(self, neighborhood):
         x, y = self.sample_new_position(neighborhood)
@@ -24,7 +26,10 @@ class Particle(Entity):
         self.update_p_best()
        
     def sample_new_position(self, neighborhood):
-        return (0,0)
+        self.velocity_x = self.weight*self.velocity_x + self.c1 * random.uniform(0, 1) * (self.p_best_x - self.x) + self.c2 * random.uniform(0, 1) * (neighborhood.velocity_x)
+        self.velocity_y = self.weight*self.velocity_y + self.c1 * random.uniform(0, 1) * (self.p_best_y - self.y) + self.c2 * random.uniform(0, 1) * (neighborhood.velocity_y)
+        return (self.velocity_x + self.x, self.velocity_y + self.y)
+
 
     def update_position(self, x, y):
         self.x = x
--- ParticleSwarmOptimisation/ParticleSwarmOptimizer.py
+++ ParticleSwarmOptimisation/ParticleSwarmOptimizer.py
@@ -1,36 +1,36 @@
 import sys
 sys.path.insert(1, '.')
-
 from Optimisation.Optimizer import Optimizer
 from ParticleSwarmOptimisation.Particle import Particle
+
+
 class ParticleSwarmOptimizer(Optimizer):
 
-     def __init__(self, objective_function, population_size, iteration_number):
+    def __init__(self, objective_function, population_size, iteration_number, weight, c1, c2):
         super.__init__(objective_function, population_size, iteration_number)
-        self.g_best_x = -1
-        self.g_best_y = -1
-        self.g_best_fitness = -1
-      
+        self.g_best_particle = None
+        self.weight = weight
+        self.c1 = c1
+        self.c2 = c2
+
     def initialize_swarm(self):
         self.initialize_particles()
 
     def initialize_particles(self):
-        self.particles = [Optimizer(self.objective_function) for i in range(int(self.population_size))]
-
+        self.particles = [Particle(self.objective_function, self.weight, self.c1, self.c2) for i in range(int(self.population_size))]
 
     def release_the_swarm(self):
         for i in range(len(self.iteration_number)):
             self.find_g_best()
             self.explore()
             
-    def find_g_best():
-        # max_prob = max(e.probability for e in self.employeed)
-        # best_bee = list(filter(lambda b: b.probability == max_prob, self.employeed))
-
-
-    def explore():
+    def find_g_best(self):
+        max_prob = max(e.fitness for e in self.particles)
+        self.g_best_particle = list(filter(lambda b: b.fitness == max_prob, self.particles))
+        
+    def explore(self):
         for i in range(len(self.particles)):
-            self.particles[i].explore_neighborhood(None)
+            self.particles[i].explore_neighborhood(self.g_best_particle)
 
 
 
